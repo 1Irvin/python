e640a32b7a6d839982bdb189008ff8fd0b17512c
1Irvin <irvinmutuma97@gmail.com>
2024-02-18 17:40:49 +0300

    ,,,

---
+++ alphabetical.py
@@ -0,0 +1,12 @@
+def names():
+    print("Please enter your Full name.")
+    first=str(input())
+    second=str(input())
+    full=first+second
+ 
+    return names
+
+
+print("Please enter the number of members present")
+members=int(input())
+
